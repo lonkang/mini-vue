5713125d14983934d464ee2e6d5ec4cab4dc0ead
longkang <1975181337@qq.com>
2022-04-28 22:41:28 +0800

    feat: 实现effect函数的stop和onStop功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,14 +1,38 @@
+import { extend } from "./shared"
+
 class ReactiveEffect {
-  private _fn
-  constructor(fn, public scheduler?) {
+  private _fn: any
+  deps = []
+  active = true
+  onStop?: () => void
+  public scheduler: Function | undefined
+  constructor(fn,  scheduler) {
     this._fn = fn
+    this.scheduler = scheduler
   }
   run() {
-    activeEffect = this as any
-    let res = this._fn()
-    // activeEffect = null
-    return res
+    activeEffect = this
+    return this._fn()
   }
+  stop () {
+    if(this.active) {
+      cleanupEffect(this);
+      if(this.onStop) {
+        this.onStop()
+      }
+      this.active = false
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  // 找到所有依赖这个 effect 的响应式对象
+  // 从这些响应式对象里面把 effect 给删除掉
+  effect.deps.forEach((dep) => {
+    dep.delete(effect);
+  });
+
+  effect.deps.length = 0;
 }
 
 const targetMap = new Map();
@@ -23,7 +47,9 @@ export function track(target, key) {
     dep = new Set()
     depsMap.set(key, dep)
   }
+  if(!activeEffect) return
   dep.add(activeEffect)
+  activeEffect.deps.push(dep)
 }
 export function trigger(target, key) {
   let depsMap = targetMap.get(target)
@@ -40,6 +66,12 @@ export function trigger(target, key) {
 let activeEffect
 export  function effect (fn, options:any = {}){
   let _effect = new ReactiveEffect(fn, options.scheduler)
+  extend(_effect, options)
   _effect.run()
-  return _effect.run.bind(_effect)
+  const runner : any = _effect.run.bind(_effect)
+  runner.effect = _effect
+  return runner
+}
+export function stop(runner) {
+  runner.effect.stop()
 }
