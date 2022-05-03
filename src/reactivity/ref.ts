f95b7ff59bdbdde154a81616873cb25275b1c35e
longkang <1975181337@qq.com>
2022-05-03 22:48:46 +0800

    'feat:添加proxyRefs功能'

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -4,9 +4,12 @@ import { reactive } from "./reactive";
 
 class RefImpl {
   private _value: any;
+  // 获取到的原始值
   private _rawValue: any;
+  // 依赖收集
   public dep;
-  public _v_isRef = true
+  // 代表是一个ref
+  public _v_isRef = true;
   constructor(value) {
     this._rawValue = value;
     // 如果传递过来的是对象的话 就用reactive包裹一下
@@ -22,7 +25,7 @@ class RefImpl {
   }
   set value(newValue) {
     if (hasChanged(newValue, this._rawValue)) {
-      this._rawValue = newValue
+      this._rawValue = newValue;
       this._value = convert(newValue);
       // 派发更新
       triggerEffects(this.dep);
@@ -30,7 +33,7 @@ class RefImpl {
   }
 }
 function convert(value) {
-  return  isObject(value) ? reactive(value) : value;
+  return isObject(value) ? reactive(value) : value;
 }
 function trackRefValue(ref) {
   if (isTracking()) {
@@ -43,9 +46,26 @@ export function ref(value) {
 }
 
 export function isRef(value) {
-  return !!value._v_isRef
+  return !!value._v_isRef;
 }
 
-export function unRef(value) {
-  return isRef(value) ? value.value : value
+export function unRef(ref) {
+  return isRef(ref) ? ref.value : ref;
+}
+// 自动解构ref.value
+export function proxyRefs(value) {
+  return new Proxy(value, {
+    // 自动解构 ref.value
+    get(target, key) {
+      return unRef(Reflect.get(target, key));
+    },
+    set(target, key, value) {
+      // 如果当前的target[key]是ref 且新值不是ref 就直接修改ref.value的值
+      if (isRef(target[key]) && !isRef(value)) {
+        return (target[key].value = value);
+      } else {
+        return Reflect.set(target, key, value);
+      }
+    },
+  });
 }
