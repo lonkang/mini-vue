76249a19a35a3bce1eb1c7e679704d5539a3e799
lonkang <1975181337@qq.com>
2022-08-03 23:19:13 +0800

    feat: diff添加最长递增子序列

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -40,26 +40,69 @@ import { h, ref } from "../../lib/mini-vue.esm.js";
 
 // // ABCEDFG -> ABECFG
 // {
+// const prevChildren = [
+//   h("span", { key: "A" }, "A"),
+//   h("span", { key: "B" }, "B"),
+//   h("span", { key: "C", id: 'c-prev' }, "C"),
+//   h("span", { key: "E" }, "E"),
+//   h("span", { key: "D" }, "D"),
+//   h("span", { key: "F" }, "F"),
+//   h("span", { key: "G" }, "G"),
+// ];
+// const nextChildren = [
+//   h("span", { key: "A" }, "A"),
+//   h("span", { key: "B" }, "B"),
+//   h("span", { key: "E" }, "E"),
+//   h("span", { key: "C", id: 'c-next' }, "C"),
+//   h("span", { key: "F" }, "F"),
+//   h("span", { key: "G" }, "G"),
+// ];
+
+// const prevChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C" }, "C"),
+//   h("p", { key: "E" }, "E"),
+//   h("p", { key: "F" }, "F"),
+//   h("p", { key: "G" }, "G"),
+// ];
+
+// const nextChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "E" }, "E"),
+//   h("p", { key: "C" }, "C"),
+//   h("p", { key: "D" }, "D"),
+//   h("p", { key: "F" }, "F"),
+//   h("p", { key: "G" }, "G"),
+// ];
+
+// 综合例子
+// A,B(C,D,E,Z)F,G
+// A,B(D,C,Y,E)F,G
 const prevChildren = [
-  h("span", { key: "A" }, "A"),
-  h("span", { key: "B" }, "B"),
-  h("span", { key: "C", id: 'c-prev' }, "C"),
-  h("span", { key: "E" }, "E"),
-  h("span", { key: "D" }, "D"),
-  h("span", { key: "F" }, "F"),
-  h("span", { key: "G" }, "G"),
-];
-const nextChildren = [
-  h("span", { key: "A" }, "A"),
-  h("span", { key: "B" }, "B"),
-  h("span", { key: "E" }, "E"),
-  h("span", { key: "C", id: 'c-next' }, "C"),
-  h("span", { key: "F" }, "F"),
-  h("span", { key: "G" }, "G"),
+  h("p", { key: "A" }, "A"),
+  h("p", { key: "B" }, "B"),
+  h("p", { key: "C" }, "C"),
+  h("p", { key: "D" }, "D"),
+  h("p", { key: "E" }, "E"),
+  h("p", { key: "Z" }, "Z"),
+  h("p", { key: "F" }, "F"),
+  h("p", { key: "G" }, "G"),
 ];
 
+const nextChildren = [
+  h("p", { key: "A" }, "A"),
+  h("p", { key: "B" }, "B"),
+  h("p", { key: "D" }, "D"),
+  h("p", { key: "C" }, "C"),
+  h("p", { key: "Y" }, "Y"),
+  h("p", { key: "E" }, "E"),
+  h("p", { key: "F" }, "F"),
+  h("p", { key: "G" }, "G"),
+];
 export default {
-  name: "ArrayToText",
+  name: "ArrayToArray",
   render() {
     const self = this;
     return self.isChange === true
--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -9,7 +9,7 @@ export function createRenderer(options) {
   const {
     createElement: hostCreateElement,
     patchProp: hostPatchProp,
-    insert,
+    insert: hostInsert,
     remove: hostRemove,
     setElementText: hostSetElementText,
   } = options;
@@ -54,10 +54,10 @@ export function createRenderer(options) {
     if (!n1) {
       mountElement(n2, container, parentComponent, anchor);
     } else {
-      patchElement(n1, n2, container, parentComponent, anchor);
+      patchElement(n1, n2, parentComponent, anchor);
     }
   }
-  function patchElement(n1, n2, container, parentComponent, anchor) {
+  function patchElement(n1, n2, parentComponent, anchor) {
     console.log("patchElement");
     console.log("n1", n1);
     console.log("n2", n2);
@@ -148,7 +148,11 @@ export function createRenderer(options) {
 
       let patched = 0; // 统计对比过的数量
       const toBePatched = e2 - s2 + 1; // 未处理的新节点剩余数量
-
+      // 创建数组的时候给定数组的长度，这个是性能最快的写法
+      // 初始化为 0 , 后面处理的时候 如果发现是 0 的话，那么就说明新值在老的里面不存在
+      let newIndexToOldIndexMap = Array(toBePatched).fill(0);
+      let moved = false;
+      let maxNewIndexSoFar = 0;
       const keyToNewMap = new Map();
       // 遍历剩余的新节点，生成一份节点key -> index 的映射表keyToNewIndexMap
       for (let i = s2; i <= e2; i++) {
@@ -157,11 +161,13 @@ export function createRenderer(options) {
           keyToNewMap.set(nextChild.key, i);
         }
       }
-      // 2. 从新节点中找出老节点
+      // 遍历老节点
+      // 1. 需要找出老节点有，而新节点没有的 -> 需要把这个节点删除掉
+      // 2. 新老节点都有的，—> 需要 patch
       for (let i = s1; i <= e1; i++) {
         let prevChild = c1[i];
 
-        // 当旧节点超过新节点的时候，直接删除节点
+        // 如果老的节点大于新节点的数量的话，那么这里在处理老节点的时候就直接删除即可
         if (patched >= toBePatched) {
           hostRemove(prevChild.el);
           continue;
@@ -184,12 +190,44 @@ export function createRenderer(options) {
           // 如果老节点中不载新节点列表中，那就删除
           hostRemove(prevChild.el);
         } else {
+          newIndexToOldIndexMap[newIndex - s2] = i + 1;
+          if (newIndex > maxNewIndexSoFar) {
+            maxNewIndexSoFar = newIndex;
+          } else {
+            moved = true;
+          }
           // 如果存在就进行patch
           patch(prevChild, c2[newIndex], container, parentComponent, null);
           patched++;
         }
       }
-      // let
+      // 获取最长递增子序列来优化逻辑
+      const increasingNewIndexSequence = moved
+        ? getSequence(newIndexToOldIndexMap)
+        : [];
+      let j = increasingNewIndexSequence.length - 1;
+      // 倒序遍历
+      for (let i = toBePatched - 1; i >= 0; i--) {
+        const nextIndex = s2 + i;
+        const nextChild = c2[nextIndex];
+        const anchor = nextIndex + 1 < l2 ? c2[nextIndex + 1].el : null;
+
+        if (newIndexToOldIndexMap[i] === 0) {
+          // 新的在老的中不存在 就直接创建
+          patch(null, nextChild, container, parentComponent, anchor);
+        } else if (moved) {
+          // 需要移动
+          // 1. j 已经没有了 说明剩下的都需要移动了
+          // 2. 最长子序列里面的值和当前的值匹配不上， 说明当前元素需要移动
+          if (j < 0 || increasingNewIndexSequence[j] !== i) {
+            hostInsert(nextChild.el, container, anchor);
+          } else {
+            // 这里就是命中了  index 和 最长递增子序列的值
+            // 所以可以移动指针了
+            j--;
+          }
+        }
+      }
     }
   }
   function unMountChildren(children) {
@@ -234,7 +272,7 @@ export function createRenderer(options) {
       // 初始化传递null
       hostPatchProp(el, key, null, val);
     }
-    insert(el, container, anchor);
+    hostInsert(el, container, anchor);
 
     // container.append(el);
   }
@@ -275,4 +313,44 @@ export function createRenderer(options) {
   return {
     createApp: createAppAPI(render),
   };
+}
+function getSequence(arr: number[]): number[] {
+  const p = arr.slice();
+  const result = [0];
+  let i, j, u, v, c;
+  const len = arr.length;
+  for (i = 0; i < len; i++) {
+    const arrI = arr[i];
+    if (arrI !== 0) {
+      j = result[result.length - 1];
+      if (arr[j] < arrI) {
+        p[i] = j;
+        result.push(i);
+        continue;
+      }
+      u = 0;
+      v = result.length - 1;
+      while (u < v) {
+        c = (u + v) >> 1;
+        if (arr[result[c]] < arrI) {
+          u = c + 1;
+        } else {
+          v = c;
+        }
+      }
+      if (arrI < arr[result[u]]) {
+        if (u > 0) {
+          p[i] = result[u - 1];
+        }
+        result[u] = i;
+      }
+    }
+  }
+  u = result.length;
+  v = result[u - 1];
+  while (u-- > 0) {
+    result[u] = v;
+    v = p[v];
+  }
+  return result;
 }
